4388ad4ff56bfbb04348927525f0cda715917fb2
Nola Smith <smith.nola1@gmail.com>
2020-05-11 15:37:39 -0400

    Reorganized

--- src/main/java/AppInteraction.java
+++ src/main/java/AppInteraction.java
@@ -1,7 +1,7 @@
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class UserInteraction {
+public class MainInteraction {
     private static Scanner keyboard = new Scanner(System.in);
     private static String choice;
     //decision to play or create
