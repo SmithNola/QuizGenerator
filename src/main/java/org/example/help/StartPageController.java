be91348e22223691904f93cf6340fb4b6c98f4e7
Nola Smith <smith.nola1@gmail.com>
2020-05-23 01:29:11 -0400

    created start page

---
+++ src/main/java/org/example/help/StartPageController.java
@@ -0,0 +1,16 @@
+package org.example.help;
+
+import javafx.fxml.FXML;
+
+public class StartPageController {
+
+    @FXML
+    private void switchToLogin(){
+
+    }
+
+    @FXML
+    private void switchToSignUp(){
+
+    }
+}
