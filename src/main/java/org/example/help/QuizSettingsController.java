4351096cf26ca4bbc6ad8f9e427156a451666a8b
Nola Smith <smith.nola1@gmail.com>
2020-05-25 12:59:44 -0400

    Layout of QuizSettings created

--- src/main/java/org/example/help/QuizSettingsController.java
+++ src/main/java/org/example/help/QuizSettingsController.java
@@ -1,4 +1,12 @@
 package org.example.help;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class QuizSettingsController {
+    @FXML
+    private void switchToCreate() throws IOException {
+        App.setRoot("create");
+    }
 }
