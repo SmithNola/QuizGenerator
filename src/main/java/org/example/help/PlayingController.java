ad9217934eec7ed8125c5225b94253e1de1e95c4
Nola Smith <smith.nola1@gmail.com>
2020-05-25 12:38:25 -0400

    Finished creating the playing track outline

--- src/main/java/org/example/help/PlayingController.java
+++ src/main/java/org/example/help/PlayingController.java
@@ -1,4 +1,12 @@
 package org.example.help;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class PlayingController {
+    @FXML
+    private void switchToScore() throws IOException {
+        App.setRoot("score");
+    }
 }
--- src/main/java/org/example/help/QuizInfoController.java
+++ src/main/java/org/example/help/QuizInfoController.java
@@ -1,4 +1,12 @@
 package org.example.help;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class QuizInfoController {
+    @FXML
+    private void switchToPlaying() throws IOException {
+        App.setRoot("playing");
+    }
 }
