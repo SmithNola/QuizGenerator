19f3def57b44679c156c6bfbd6e4ccbdd7a9cf9b
Nola Smith <smith.nola1@gmail.com>
2020-05-27 02:41:56 -0400

    Will log you into app

--- src/main/java/org/example/help/LoginController.java
+++ src/main/java/org/example/help/LoginController.java
@@ -1,12 +1,30 @@
 package org.example.help;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class LoginController {
+
     @FXML
-    private void switchToHomePage() throws IOException {
-        App.setRoot("homePage");
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private Text errorMessage;
+
+    @FXML
+    private void switchToHomePage() throws IOException, SQLException {
+        String loginName;
+        loginName = DatabaseConnection.checkLogin(username.getText(),password.getText());
+        if(loginName == null){
+            errorMessage.setText("Wrong username/password combination");
+        }
+        else{
+            App.setRoot("homePage");
+        }
     }
 }
