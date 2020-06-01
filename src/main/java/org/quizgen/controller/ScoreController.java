a9453f13ec4c3a1724705386ac4269ea2eb4403d
Nola Smith <smith.nola1@gmail.com>
2020-05-31 20:32:57 -0400

    Will display score

--- src/main/java/org/quizgen/controller/ScoreController.java
+++ src/main/java/org/quizgen/controller/ScoreController.java
@@ -1,11 +1,20 @@
 package org.quizgen.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.quizgen.App;
 
 import java.io.IOException;
 
 public class ScoreController {
+
+    @FXML
+    private Label score;
+
+    public void initialize(){
+        score.setText(String.valueOf(PlayingController.getScore()) + "%");
+    }
+
     @FXML
     private void switchToPlayView() throws IOException {
         App.setRoot("playview");
--- src/main/java/org/quizgen/data/DatabaseConnection.java
+++ src/main/java/org/quizgen/data/DatabaseConnection.java
@@ -34,6 +34,26 @@ public class DatabaseConnection {
         }
     }
 
+    public static boolean checkIfPlayed(String username, int quizId) throws SQLException{
+        String query = "SELECT * FROM quiz_played WHERE player_id = ? AND quiz_id = ?;";
+        PreparedStatement st = conn.prepareStatement(query);
+        int id = getPlayerId(username);
+        st.setInt(1,id);
+        st.setInt(2,quizId);
+        ResultSet results = st.executeQuery();
+        return results.next();
+    }
+
+    public static void saveScore(int score, String username, int quizId) throws SQLException {
+            String query = "INSERT INTO quiz_played (player_id, quiz_id, score) VALUES (?, ?, ?)";
+            PreparedStatement st = conn.prepareStatement(query);
+            int id = getPlayerId(username);
+            st.setInt(1, id);
+            st.setInt(2, quizId);
+            st.setInt(3, score);
+            st.executeUpdate();
+    }
+
     //Will check if username already exists in database
     public static boolean checkUsername(String username) throws SQLException {
         String query = "SELECT username FROM player WHERE username = ?";
