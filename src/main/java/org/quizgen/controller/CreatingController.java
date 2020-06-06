d2096b8ff3b5e47634e4e6d48779b41cb4633816
Nola Smith <smith.nola1@gmail.com>
2020-06-06 12:46:37 -0400

    Will save to the database but code still needs cleaning

--- src/main/java/org/quizgen/controller/CreatingController.java
+++ src/main/java/org/quizgen/controller/CreatingController.java
@@ -1,17 +1,20 @@
 package org.quizgen.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.quizgen.App;
+import org.quizgen.data.DatabaseConnection;
+import org.quizgen.model.Question;
 import org.quizgen.model.Quiz;
-
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CreatingController {
 
@@ -21,6 +24,7 @@ public class CreatingController {
     @FXML
     private VBox overall;
     private int count = 1;
+    private ArrayList<VBox> questions = new ArrayList<>();
 
     public void initialize(){
         quiz=CreateViewController.getClickedQuiz();
@@ -34,6 +38,7 @@ public class CreatingController {
         Label questionNum = new Label(count + ": ");
         TextField question = new TextField();
         Button addNew = new Button("+");
+        ToggleGroup group = new ToggleGroup();
         addNew.setOnAction(new EventHandler<ActionEvent>() {//Will add a new question with choice
 
             @Override
@@ -46,27 +51,31 @@ public class CreatingController {
         deleteOld.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent actionEvent){
+                questions.remove(questionWithChoice);
                 overall.getChildren().remove(questionWithChoice);
             }
         });
         questionTracker.getChildren().addAll(questionNum,question,addNew,deleteOld);//creates number + question + button
         questionWithChoice.getChildren().addAll(questionTracker);
+        questions.add(questionWithChoice);
         overall.getChildren().add(questionWithChoice);
-        newChoice(questionWithChoice);
+        newChoice(questionWithChoice,group);
         count++;
     }
 
-    private void newChoice(VBox questionWithChoice){
+    private void newChoice(VBox questionWithChoice,ToggleGroup group){
         HBox choiceTracker = new HBox();
         TextField choice = new TextField();
         choice.setMaxWidth(80);
         Button addNew = new Button("+");
+        RadioButton answer = new RadioButton();
+        answer.setToggleGroup(group);
         addNew.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent arg0){
                 addNew.setVisible(false);
-                newChoice(questionWithChoice);
+                newChoice(questionWithChoice,group);
             }
         } );
         Button deleteOld = new Button("-");
@@ -76,12 +85,48 @@ public class CreatingController {
                 questionWithChoice.getChildren().remove(choiceTracker);
             }
         });
-        choiceTracker.getChildren().addAll(choice,addNew,deleteOld);
+        choiceTracker.getChildren().addAll(choice,answer,addNew,deleteOld);
         questionWithChoice.getChildren().add(choiceTracker);
     }
 
+    private ArrayList<Question> retrieveQuestions(){
+        ArrayList<Question> allQuestions = new ArrayList<>();
+        for(int i = 0; i <questions.size(); i++){
+            Question savedQuestion = new Question();
+            ArrayList<String> choices = new ArrayList<>();
+            VBox questionWithChoice = questions.get(i);//gets question with choice
+            ObservableList<Node> eachRow = questionWithChoice.getChildren();//saves each HBox
+            for(int j = 0; j<eachRow.size();j++){//will go through each Hbox
+                HBox row = (HBox) eachRow.get(j);//gets first HBox
+                ObservableList<Node> eachElement = row.getChildren();//saves each elements
+                if(j == 0){
+                    TextField question = (TextField) eachElement.get(1);//saves text field
+                    savedQuestion.setName(question.getText());
+                }else{
+                    TextField choice = (TextField) eachElement.get(0);//saves text field
+                    RadioButton answer = (RadioButton) eachElement.get(1);
+                    if(answer.isSelected() == true){
+                        savedQuestion.setAnswer(j);
+                    }
+                    choices.add(choice.getText());
+                }
+            }
+            savedQuestion.setChoices(choices);
+            allQuestions.add(savedQuestion);
+        }
+        return allQuestions;
+    }
+
+    private void saveQuiz() throws SQLException{
+        quiz.setQuestions(retrieveQuestions());
+        quiz.setCreator(LoginController.getUsername());
+
+        DatabaseConnection.saveQuiz(quiz);
+    }
+
     @FXML
-    private void switchToCreateView() throws IOException {
+    private void switchToCreateView() throws IOException, SQLException{
+        saveQuiz();
         App.setRoot("createView");
     }
 }
--- src/main/java/org/quizgen/controller/QuizSettingsController.java
+++ src/main/java/org/quizgen/controller/QuizSettingsController.java
@@ -1,13 +1,52 @@
 package org.quizgen.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import org.quizgen.App;
+import org.quizgen.model.Quiz;
 
 import java.io.IOException;
 
 public class QuizSettingsController {
+
+    private Quiz quiz;
+    @FXML
+    private TextField quizName;
+    @FXML
+    private TextField quizGenre;
+    @FXML
+    private RadioButton yes;
+    @FXML
+    private RadioButton no;
+
+    public void initialize(){
+        ToggleGroup group = new ToggleGroup();
+        yes.setToggleGroup(group);
+        no.setToggleGroup(group);
+    }
+
+    private void saveProperties(){
+        quiz = CreateViewController.getClickedQuiz();
+        quiz.setName(quizName.getText());
+        quiz.setGenre(quizGenre.getText());
+        if(yes.isSelected() == true){
+            quiz.setOrdered(1);
+        }
+        else{
+            quiz.setOrdered(0);
+        }
+    }
+
+    public Quiz getQuiz(){
+        return quiz;
+    }
+
     @FXML
     private void switchToCreate() throws IOException {
+        saveProperties();
         App.setRoot("creating");
     }
 }
