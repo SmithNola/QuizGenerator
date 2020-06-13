a1c0baa20d6e54a144dec3275678b149f204823a
Nola Smith <smith.nola1@gmail.com>
2020-06-13 15:53:25 -0400

    Will check if previous page was PlayView or CreateView

--- src/main/java/org/quizgen/controller/QuizInfoController.java
+++ src/main/java/org/quizgen/controller/QuizInfoController.java
@@ -1,10 +1,13 @@
 package org.quizgen.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.quizgen.App;
 import org.quizgen.model.Quiz;
-
 import java.io.IOException;
 
 public class QuizInfoController{
@@ -15,20 +18,44 @@ public class QuizInfoController{
     @FXML
     private Text numText = new Text();
     @FXML
+    private VBox overall;
+    @FXML
     public void initialize(){
-        Quiz quiz = PlayViewController.getClickedQuiz();
-        nameText.setText(quiz.getName());
-        creatorText.setText(quiz.getCreator());
-        numText.setText(String.valueOf(quiz.getNumberOfQuestions()));
-    }
-
-    @FXML
-    private void switchToPlaying() throws IOException {
-        App.setRoot("playing");
-    }
-
-    @FXML
-    private void switchToEditing() throws IOException{
-        App.setRoot("quizSettings");
+        //Will check if previous page was PlayView or CreateView
+        if(PlayViewController.getClickedQuiz().getQuizId() != 0){
+            Quiz quiz = PlayViewController.getClickedQuiz();
+            nameText.setText(quiz.getName());
+            creatorText.setText(quiz.getCreator());
+            numText.setText(String.valueOf(quiz.getNumberOfQuestions()));
+            Button play = new Button("Play");
+            play.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent arg0) {
+                    try{
+                        App.setRoot("playing");
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            } );
+            overall.getChildren().add(play);
+        }else{
+            Quiz quiz = CreateViewController.getClickedQuiz();
+            nameText.setText(quiz.getName());
+            creatorText.setText(quiz.getCreator());
+            numText.setText(String.valueOf(quiz.getNumberOfQuestions()));
+            Button edit = new Button("Edit");
+            edit.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent arg0) {
+                    try{
+                        App.setRoot("quizSettings");
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            } );
+            overall.getChildren().add(edit);
+        }
     }
 }
